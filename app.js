9d2ddbac1a4d616683ec895a17a7170dde7cb325
DeekshaChelamalla25 <112186227+DeekshaChelamalla25@users.noreply.github.com>
2022-11-02 22:44:48 -0500

    updated

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var selectorRouter = require('./routes/selector');
+var gridbuildRouter = require('./routes/gridbuild');
+var employeeRouter = require('./routes/employee');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+app.use('/employee', employeeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
